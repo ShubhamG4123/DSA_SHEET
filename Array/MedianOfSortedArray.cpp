46c650241969751ee2a3927f3c1f5bce6ba3fc5c
ShubhamG4123 <100066162+ShubhamG4123@users.noreply.github.com>
2023-02-13 21:24:41 +0530

    Array

---
+++ Array/MedianOfSortedArray.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+	public:
+		int find_median(vector<int> v)
+		{
+		    // Code here.
+		    sort(v.begin(),v.end());
+		    
+		    int n = v.size();
+		    int i = n/2;
+		    if(n%2==0)
+		    {
+		        return (v[i]+v[i-1])/2;
+		    }
+		    else
+		    {
+		        return v[i];
+		    }
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>v[i];
+    	Solution ob;
+    	int ans = ob.find_median(v);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
