7d98b93fa9c47f739c43597cf4c161d45e35ce59
ShubhamG4123 <100066162+ShubhamG4123@users.noreply.github.com>
2023-02-23 13:22:16 +0530

    String

---
+++ Strings/Palindrome.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    // Your code goes here
+	    int n = S.size();
+	    int low = 0;
+	    int high = n-1;
+	    
+	    while(high >=low)
+	    {
+	        if(S[low]==S[high])
+	        {
+	            low++;
+	            high--;
+	        }
+	        else
+	        {
+	            return 0;
+	        }
+	    }
+	    return 1;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
